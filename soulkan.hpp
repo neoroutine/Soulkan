e3ee1a7a01b69f613dadb23bb01314913fb4f630
Sabry Chasseray <sabry.chasseray@protonmail.com>
2022-11-28 00:04:14 +0100

    Update soulkan.hpp

--- soulkan.hpp
+++ soulkan.hpp
@@ -206,7 +206,7 @@ namespace SOULKAN_NAMESPACE
 				std::cout << "Waiting for " << operationName << std::endl;
 			}
 			
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 			var = operationsStatus.get()[operationName];
 		}
 	}
